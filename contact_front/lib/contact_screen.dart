5f89a11dd06666bf8fcd490f02ef1d6e673be5c6
thiyangi28 <thiyangip24@gmail.com>
2026-04-14 21:03:09 +0530

    feat:add contact screen

---
+++ contact_front/lib/contact_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class AddContactScreen extends StatelessWidget {
+  const AddContactScreen({super.key});
+
+  @override
+  State<AddContactScreen> createState() => _AddContactScreenState();
+  }
+  class _AddContactScreenState extends State<AddContactScreen> {
+   
+}
